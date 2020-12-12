8fecbc12256fcf2dbde56c0d73c8e550d7348e2c
Hyeon Jae Ho <44640159+jaeho214@users.noreply.github.com>
2020-12-12 22:23:18 +0900

    예치금 반환 추가

--- core/src/main/java/org/nhhackaton/api/p2p/dto/DepositReturnRequest.java
+++ core/src/main/java/org/nhhackaton/api/p2p/dto/DepositReturnRequest.java
@@ -1,12 +1,15 @@
 package org.nhhackaton.api.p2p.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.nhhackaton.api.HeaderRequestParent;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor @Builder
 public class DepositReturnRequest extends HeaderRequestParent {
     @JsonProperty("P2pCmtmNo")
     private String P2pCmtmNo;
